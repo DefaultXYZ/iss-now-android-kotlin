0d7bbf6264bee0feee6fac1b060648abf5d96a37
Yevhenii Ishchenko <yevhenii.i@hexagen48.com>
2018-06-28 03:01:39 +0200

    Add methods to DAO, remove example tests

--- app/src/main/java/com/defaultxyz/issnow/data/db/IssDao.kt
+++ app/src/main/java/com/defaultxyz/issnow/data/db/IssDao.kt
@@ -1,17 +1,17 @@
 package com.defaultxyz.issnow.data.db
 
 import android.arch.lifecycle.LiveData
-import android.arch.persistence.room.Dao
-import android.arch.persistence.room.OnConflictStrategy
-import android.arch.persistence.room.Query
-import android.arch.persistence.room.Update
+import android.arch.persistence.room.*
 import com.defaultxyz.issnow.data.model.IssAstro
 import com.defaultxyz.issnow.data.model.IssPosition
 
 @Dao
 interface IssDao {
-    @Update(onConflict = OnConflictStrategy.REPLACE)
-    fun updatePosition(position: IssPosition)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertLastPosition(position: IssPosition)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAstros(astros: List<IssAstro>)
 
     @Query("SELECT * FROM tb_iss_position ORDER BY timestamp LIMIT 1")
     fun getLastPosition(): LiveData<IssPosition>
@@ -21,4 +21,7 @@ interface IssDao {
 
     @Query("SELECT * FROM tb_iss_astro WHERE issId = :issId")
     fun getAstros(issId: Int): LiveData<List<IssAstro>>
+
+    @Query("DELETE FROM tb_iss_astro")
+    fun clearAstros()
 }
