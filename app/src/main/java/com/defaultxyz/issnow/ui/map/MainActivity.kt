28faba3e0ec156d9e4dde3d74cd81b172f692b21
Yevhenii Ishchenko <yevhenii.i@hexagen48.com>
2018-06-28 08:24:13 +0200

    Implement abstract MapActivity

--- app/src/main/java/com/defaultxyz/issnow/ui/map/MainActivity.kt
+++ app/src/main/java/com/defaultxyz/issnow/ui/map/MainActivity.kt
@@ -1,53 +1,20 @@
 package com.defaultxyz.issnow.ui.map
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import com.defaultxyz.issnow.R
-import kotlinx.android.synthetic.main.activity_main.*
+import com.defaultxyz.issnow.ui.utils.MapActivity
+import com.mapbox.mapboxsdk.maps.MapboxMap
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : MapActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        map_view.onCreate(savedInstanceState)
-        map_view.getMapAsync {
-            
-        }
+        super.onCreateMap(savedInstanceState)
     }
 
-    override fun onSaveInstanceState(outState: Bundle?) {
-        super.onSaveInstanceState(outState)
-        map_view.onSaveInstanceState(outState!!)
-    }
+    override fun onMapReady(mapboxMap: MapboxMap?) {
+        if (mapboxMap == null) return
 
-    override fun onLowMemory() {
-        super.onLowMemory()
-        map_view.onLowMemory()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        map_view.onStart()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        map_view.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        map_view.onPause()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        map_view.onStop()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        map_view.onDestroy()
     }
 }
