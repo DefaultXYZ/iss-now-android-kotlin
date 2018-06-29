595c80b94704ed870304070089a92f5739fe3da2
Yevhenii Ishchenko <yevhenii.i@hexagen48.com>
2018-06-29 08:37:35 +0200

    Provide repository to ViewModel

--- app/src/main/java/com/defaultxyz/issnow/ui/map/MainActivityViewModel.kt
+++ app/src/main/java/com/defaultxyz/issnow/ui/map/MainActivityViewModel.kt
@@ -1,25 +1,15 @@
 package com.defaultxyz.issnow.ui.map
 
 import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 import android.arch.lifecycle.ViewModelProvider
-import com.defaultxyz.issnow.data.model.IssAstro
+import com.defaultxyz.issnow.data.AppRepository
 import com.defaultxyz.issnow.data.model.IssPosition
 import javax.inject.Inject
 import javax.inject.Singleton
 
-class MainActivityViewModel : ViewModel() {
-    private var position = MutableLiveData<IssPosition>()
-
-    init {
-        val sample = IssPosition(0, 52.1436363, 21.0538923)
-        sample.astros = listOf(
-                IssAstro("Astro1", 1),
-                IssAstro("Astro2", 1)
-        )
-        position.postValue(sample)
-    }
+class MainActivityViewModel(repository: AppRepository) : ViewModel() {
+    private var position = repository.getLastPosition()
 
     fun getLastPosition(): LiveData<IssPosition> {
         return position
@@ -27,9 +17,11 @@ class MainActivityViewModel : ViewModel() {
 
     @Suppress("UNCHECKED_CAST")
     @Singleton
-    class Factory @Inject constructor() : ViewModelProvider.Factory {
+    class Factory @Inject constructor(private val repository: AppRepository)
+        : ViewModelProvider.Factory {
+
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            return MainActivityViewModel() as T
+            return MainActivityViewModel(repository) as T
         }
     }
 }
