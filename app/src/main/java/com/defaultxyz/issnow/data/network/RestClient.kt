340eea075d11b66fa23ae2539c45a8791df40aa0
Yevhenii Ishchenko <yevhenii.i@hexagen48.com>
2018-06-29 13:30:42 +0200

    Implement Rest worker and client

--- app/src/main/java/com/defaultxyz/issnow/data/network/RestClient.kt
+++ app/src/main/java/com/defaultxyz/issnow/data/network/RestClient.kt
@@ -6,4 +6,19 @@ import javax.inject.Singleton
 @Singleton
 class RestClient @Inject constructor(private val api: IssNotifyApi) {
 
+    fun getIssPosition(): String? {
+        val response = api.getIssNow().execute()
+        if (response.isSuccessful) {
+            return response.body()?.string()
+        }
+        return null
+    }
+
+    fun getAstros(): String? {
+        val response = api.getAstros().execute()
+        if (response.isSuccessful) {
+            return response.body()?.string()
+        }
+        return null
+    }
 }
--- app/src/main/java/com/defaultxyz/issnow/data/network/RestWorker.kt
+++ app/src/main/java/com/defaultxyz/issnow/data/network/RestWorker.kt
@@ -1,16 +1,41 @@
 package com.defaultxyz.issnow.data.network
 
+import android.os.Handler
+import androidx.work.Data
 import androidx.work.Worker
 import com.defaultxyz.issnow.MainApplication
+import com.defaultxyz.issnow.data.model.response.AstrosResponse
 import javax.inject.Inject
 
 class RestWorker : Worker() {
-
     @Inject lateinit var restClient: RestClient
 
     override fun doWork(): Result {
         (applicationContext as MainApplication).injector.inject(this)
+        val data = Data.Builder()
+        val astrosJson = restClient.getAstros()
+        data.putString(AstrosResponse::class.java.simpleName, astrosJson)
+
+        val handler = Handler()
+        handler.post(IssPositionTask(handler, data))
 
         return Result.SUCCESS
+    }
+
+    companion object {
+        const val TAG = "RestStatus"
+        const val WORK_NAME = "com.defaultxyz.issnow.RestWorker"
+    }
+
+    private inner class IssPositionTask(private val handler: Handler,
+                                        private val data: Data.Builder) : Runnable {
+        private val interval = 5000L
+
+        override fun run() {
+            val issPositionJson = restClient.getIssPosition()
+            data.putString(AstrosResponse::class.java.simpleName, issPositionJson)
+            outputData = data.build()
+            handler.postDelayed(this, interval)
+        }
     }
 }
